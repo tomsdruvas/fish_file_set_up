a88f992f06cc9187f37e6f7cbbd12b4a218abf03
Toms Druvaskalns <tomsdruvas@gmail.com>
2021-11-25 11:33:59 +0000

    made the first test

---
+++ src/fish.py
@@ -0,0 +1,6 @@
+class Fish:
+    def __init__(self, name, size, breed):
+        self.name = name
+        self.size = size
+        self.breed = breed
+        
--- tests/fish_test.py
+++ tests/fish_test.py
@@ -1,5 +1,9 @@
-
+import unittest
+from src.fish import Fish
 
 class TestFish(unittest.TestCase):
     def setUp(self):
-        self.
+        self.nemo = Fish("Nemo", "small", "Clown Fish")
+
+    def test_fish_has_name(self):
+        self.assertEqual("Nemo", self.nemo.name)
